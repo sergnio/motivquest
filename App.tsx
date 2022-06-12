88e7b6cf9da670d38f09206514fff6dab262c91f
sergnio <sergnioa@gmail.com>
2022-06-12 16:35:16 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+//@ts-ignore
+import logo from "./assets/potion.png";
+import { useState } from "react";
 
-export default function App() {
+export default () => {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={logo} style={styles.potion} />
+      <Text style={styles.header1}>
+        Keep track of your main activities which affect your motivation :)
+      </Text>
+
+      <View>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(newText) => setText(newText)}
+          defaultValue={text}
+        />
+        <TouchableOpacity
+          onPress={() => alert("Hello, world!")}
+          style={{ backgroundColor: "blue", padding: 10 }}
+        >
+          <Text style={{ fontSize: 20, color: "#fff" }}>+</Text>
+        </TouchableOpacity>
+        <Text style={{ fontSize: 20, color: "#fff" }}>-</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    // justifyContent: 'center',
   },
+  header1: {
+    color: "hsl(0,0%,0%)",
+    fontSize: 18,
+  },
+  potion: { width: 305, height: 159, resizeMode: "contain" },
 });
